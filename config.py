31e613d107df65f87da80b227dad59a44b45ad12
mzk <supermappin@gmail.com>
2026-01-02 06:45:49 +0900

    chore: backup

---
+++ config.py
@@ -0,0 +1,290 @@
+# 全て 40 session
+SESSION_CONFIG = [
+    # sub-001
+    {
+        "sub_id": "1",
+        "session_id": "sub-001_ses-01",
+        "bdf": "data/sub-001/ses-01/eeg/sub-001_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-001/ses-01/eeg/sub-001_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "1",
+        "session_id": "sub-001_ses-02",
+        "bdf": "data/sub-001/ses-02/eeg/sub-001_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-001/ses-02/eeg/sub-001_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-002
+    {
+        "sub_id": "2",
+        "session_id": "sub-002_ses-01",
+        "bdf": "data/sub-002/ses-01/eeg/sub-002_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-002/ses-01/eeg/sub-002_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "2",
+        "session_id": "sub-002_ses-02",
+        "bdf": "data/sub-002/ses-02/eeg/sub-002_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-002/ses-02/eeg/sub-002_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-003
+    {
+        "sub_id": "3",
+        "session_id": "sub-003_ses-01",
+        "bdf": "data/sub-003/ses-01/eeg/sub-003_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-003/ses-01/eeg/sub-003_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "3",
+        "session_id": "sub-003_ses-02",
+        "bdf": "data/sub-003/ses-02/eeg/sub-003_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-003/ses-02/eeg/sub-003_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-004
+    {
+        "sub_id": "4",
+        "session_id": "sub-004_ses-01",
+        "bdf": "data/sub-004/ses-01/eeg/sub-004_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-004/ses-01/eeg/sub-004_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "4",
+        "session_id": "sub-004_ses-02",
+        "bdf": "data/sub-004/ses-02/eeg/sub-004_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-004/ses-02/eeg/sub-004_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-005
+    {
+        "sub_id": "5",
+        "session_id": "sub-005_ses-01",
+        "bdf": "data/sub-005/ses-01/eeg/sub-005_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-005/ses-01/eeg/sub-005_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "5",
+        "session_id": "sub-005_ses-02",
+        "bdf": "data/sub-005/ses-02/eeg/sub-005_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-005/ses-02/eeg/sub-005_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-006
+    {
+        "sub_id": "6",
+        "session_id": "sub-006_ses-01",
+        "bdf": "data/sub-006/ses-01/eeg/sub-006_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-006/ses-01/eeg/sub-006_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "6",
+        "session_id": "sub-006_ses-02",
+        "bdf": "data/sub-006/ses-02/eeg/sub-006_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-006/ses-02/eeg/sub-006_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-007
+    {
+        "sub_id": "7",
+        "session_id": "sub-007_ses-01",
+        "bdf": "data/sub-007/ses-01/eeg/sub-007_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-007/ses-01/eeg/sub-007_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "7",
+        "session_id": "sub-007_ses-02",
+        "bdf": "data/sub-007/ses-02/eeg/sub-007_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-007/ses-02/eeg/sub-007_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-008
+    {
+        "sub_id": "8",
+        "session_id": "sub-008_ses-01",
+        "bdf": "data/sub-008/ses-01/eeg/sub-008_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-008/ses-01/eeg/sub-008_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-009
+    {
+        "sub_id": "9",
+        "session_id": "sub-009_ses-01",
+        "bdf": "data/sub-009/ses-01/eeg/sub-009_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-009/ses-01/eeg/sub-009_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "9",
+        "session_id": "sub-009_ses-02",
+        "bdf": "data/sub-009/ses-02/eeg/sub-009_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-009/ses-02/eeg/sub-009_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-010
+    {
+        "sub_id": "10",
+        "session_id": "sub-010_ses-01",
+        "bdf": "data/sub-010/ses-01/eeg/sub-010_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-010/ses-01/eeg/sub-010_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "10",
+        "session_id": "sub-010_ses-02",
+        "bdf": "data/sub-010/ses-02/eeg/sub-010_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-010/ses-02/eeg/sub-010_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-011
+    {
+        "sub_id": "11",
+        "session_id": "sub-011_ses-01",
+        "bdf": "data/sub-011/ses-01/eeg/sub-011_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-011/ses-01/eeg/sub-011_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "11",
+        "session_id": "sub-011_ses-02",
+        "bdf": "data/sub-011/ses-02/eeg/sub-011_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-011/ses-02/eeg/sub-011_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-012
+    {
+        "sub_id": "12",
+        "session_id": "sub-012_ses-01",
+        "bdf": "data/sub-012/ses-01/eeg/sub-012_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-012/ses-01/eeg/sub-012_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-013
+    {
+        "sub_id": "13",
+        "session_id": "sub-013_ses-01",
+        "bdf": "data/sub-013/ses-01/eeg/sub-013_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-013/ses-01/eeg/sub-013_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-014
+    {
+        "sub_id": "14",
+        "session_id": "sub-014_ses-01",
+        "bdf": "data/sub-014/ses-01/eeg/sub-014_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-014/ses-01/eeg/sub-014_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-015
+    {
+        "sub_id": "15",
+        "session_id": "sub-015_ses-01",
+        "bdf": "data/sub-015/ses-01/eeg/sub-015_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-015/ses-01/eeg/sub-015_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-016
+    {
+        "sub_id": "16",
+        "session_id": "sub-016_ses-01",
+        "bdf": "data/sub-016/ses-01/eeg/sub-016_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-016/ses-01/eeg/sub-016_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "16",
+        "session_id": "sub-016_ses-02",
+        "bdf": "data/sub-016/ses-02/eeg/sub-016_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-016/ses-02/eeg/sub-016_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-017
+    {
+        "sub_id": "17",
+        "session_id": "sub-017_ses-01",
+        "bdf": "data/sub-017/ses-01/eeg/sub-017_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-017/ses-01/eeg/sub-017_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "17",
+        "session_id": "sub-017_ses-02",
+        "bdf": "data/sub-017/ses-02/eeg/sub-017_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-017/ses-02/eeg/sub-017_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-018
+    {
+        "sub_id": "18",
+        "session_id": "sub-018_ses-01",
+        "bdf": "data/sub-018/ses-01/eeg/sub-018_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-018/ses-01/eeg/sub-018_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "18",
+        "session_id": "sub-018_ses-02",
+        "bdf": "data/sub-018/ses-02/eeg/sub-018_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-018/ses-02/eeg/sub-018_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-019
+    {
+        "sub_id": "19",
+        "session_id": "sub-019_ses-01",
+        "bdf": "data/sub-019/ses-01/eeg/sub-019_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-019/ses-01/eeg/sub-019_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-020
+    {
+        "sub_id": "20",
+        "session_id": "sub-020_ses-01",
+        "bdf": "data/sub-020/ses-01/eeg/sub-020_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-020/ses-01/eeg/sub-020_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-021
+    {
+        "sub_id": "21",
+        "session_id": "sub-021_ses-01",
+        "bdf": "data/sub-021/ses-01/eeg/sub-021_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-021/ses-01/eeg/sub-021_ses-01_task-meditation_events.tsv",
+    },
+
+    # sub-022
+    {
+        "sub_id": "22",
+        "session_id": "sub-022_ses-01",
+        "bdf": "data/sub-022/ses-01/eeg/sub-022_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-022/ses-01/eeg/sub-022_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "22",
+        "session_id": "sub-022_ses-02",
+        "bdf": "data/sub-022/ses-02/eeg/sub-022_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-022/ses-02/eeg/sub-022_ses-02_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "22",
+        "session_id": "sub-022_ses-03",
+        "bdf": "data/sub-022/ses-03/eeg/sub-022_ses-03_task-meditation_eeg.bdf",
+        "ev": "data/sub-022/ses-03/eeg/sub-022_ses-03_task-meditation_events.tsv",
+    },
+
+    # sub-023
+    {
+        "sub_id": "23",
+        "session_id": "sub-023_ses-01",
+        "bdf": "data/sub-023/ses-01/eeg/sub-023_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-023/ses-01/eeg/sub-023_ses-01_task-meditation_events.tsv",
+    },
+    {
+        "sub_id": "23",
+        "session_id": "sub-023_ses-02",
+        "bdf": "data/sub-023/ses-02/eeg/sub-023_ses-02_task-meditation_eeg.bdf",
+        "ev": "data/sub-023/ses-02/eeg/sub-023_ses-02_task-meditation_events.tsv",
+    },
+
+    # sub-024
+    {
+        "sub_id": "24",
+        "session_id": "sub-024_ses-01",
+        "bdf": "data/sub-024/ses-01/eeg/sub-024_ses-01_task-meditation_eeg.bdf",
+        "ev": "data/sub-024/ses-01/eeg/sub-024_ses-01_task-meditation_events.tsv",
+    },
+]
